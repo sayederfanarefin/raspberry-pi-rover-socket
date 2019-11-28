603a399991a78704059f466c703a1ccfb1035cc6
sayederfanarefin <erfanjordison@gmail.com>
2019-11-28 14:58:50 -0600

    adding ci cd

---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+echo ====> Setting up ..
+################################################################ setup docker
+
+echo ====> Install Docker ..
+
+# install prerequisites
+sudo apt-get install apt-transport-https ca-certificates software-properties-common -y
+
+# Download and install Docker.
+curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
+
+# Give the ‘pi’ user the ability to run Docker.
+sudo usermod -aG docker pi
+
+# Import Docker CPG key.
+sudo curl https://download.docker.com/linux/raspbian/gpg
+
+# Setup the Docker Repo.
+echo 'deb https://download.docker.com/linux/raspbian/ stretch stable' | sudo tee -a /etc/apt/sources.list
+
+# update and upgrade
+sudo apt-get update -y
+
+sudo apt-get upgrade -y
+
+
+# start docker
+sudo systemctl start docker.service
+
+# show docker info
+sudo docker info
+
+
+############################################################### setup IRC to enable notificaation from travis ci
